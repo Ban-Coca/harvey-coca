9302825315616e50ba629925692c0c8942ac8eda
Van Harvey Coca <vanharvey.coca@cit.edu>
2026-07-05 17:33:03 +0800

    fixed build issue

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -9,13 +9,16 @@ function Separator({
   orientation = "horizontal",
   ...props
 }: SeparatorPrimitive.Props) {
+  const resolvedClassName =
+    typeof className === "function" ? className({ orientation }) : className
+
   return (
     <SeparatorPrimitive
       data-slot="separator"
       orientation={orientation}
       className={cn(
         "shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
-        className
+        resolvedClassName
       )}
       {...props}
     />
